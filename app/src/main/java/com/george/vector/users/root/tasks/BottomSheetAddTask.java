3f5877dc6d4d82b40e61e04a70d4a5473a34f112
liggidarck <liggidarck@gmail.com>
2021-08-29 10:10:39 +0300

    Доработана страница создания заявки.
    Теперь для больших заявок текст смещается вниз.

--- app/src/main/java/com/george/vector/users/root/tasks/BottomSheetAddTask.java
+++ app/src/main/java/com/george/vector/users/root/tasks/BottomSheetAddTask.java
@@ -2,7 +2,6 @@ package com.george.vector.users.root.tasks;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,12 +9,10 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import com.george.vector.R;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
-import org.jetbrains.annotations.NotNull;
 
 public class BottomSheetAddTask extends BottomSheetDialogFragment {
 
@@ -23,10 +20,8 @@ public class BottomSheetAddTask extends BottomSheetDialogFragment {
             bar_school_new_task, bar_rucheek_new_task, bar_start_new_task;
     ImageView close_btn;
 
-    @Nullable
-    @org.jetbrains.annotations.Nullable
     @Override
-    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.bottom_sheet_add_task, container, false);
 
         ost_school_new_task = view.findViewById(R.id.ost_school_new_task);
